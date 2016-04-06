29048cf75b7bc9152f7c90dce0bfc991f4e4b41c
Maffi Craft <maffimaffi@hotmail.de>
2016-04-06 22:32:40 +0200

    GameMaster script connected
    Also ressource integers were added

--- Assets/Scripts/GameMasterScript.cs
+++ Assets/Scripts/GameMasterScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameMasterScript : MonoBehaviour {
 
+    //Main ressources of the game
+    public int res_money, res_electricity, res_workforce, res_food;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +15,13 @@ public class GameMasterScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
+
+    //Time in seconds since the app was opened last
+    public int lastDowntime { get
+        {
+            return 0;
+        }
+    }
 }
