cca39c4d7e403692033e19bcf0c8fb45c116fa4d
Maffi Craft <maffimaffi@hotmail.de>
2016-04-07 01:08:22 +0200

    Added Cloned RessourcePanels
    Including Showing the amount and auto-resizing

--- Assets/Scripts/RessourcePanelManagerScript.cs
+++ Assets/Scripts/RessourcePanelManagerScript.cs
@@ -1,24 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
-public class RessourcePanelManagerScript : MonoBehaviour {
+public class RessourcePanelManagerScript : MonoBehaviour
+{
 
-    private int ressourceAmount = 4;
-    private string[] resNames = { "resMoney", "resElectricity", "resWorkforce", "resFood" };
     public GameObject RessourcePanelPrefab;
 
-	// Use this for initialization
-	void Start () {
-	    for (int i = 0; i < ressourceAmount; i++)
-        {
-            Instantiate(RessourcePanelPrefab,new Vector3(90+ i*170,-25,0),Quaternion.identity);
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+    private int ressourceAmount = 4;
+    private GameObject[] panelArray = new GameObject[4];
 
-    GameMasterScript gameMaster { get {return GameObject.Find("GameMaster").GetComponent<GameMasterScript>(); } }
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < ressourceAmount; i++)
+        {
+            GameObject tempObject = Instantiate(RessourcePanelPrefab);
+            tempObject.transform.parent = this.transform;
+            tempObject.transform.localPosition = new Vector3(90 + i * 190, -25, 0);
+            tempObject.transform.localScale = new Vector3(1, 1, 1);
+            panelArray[i] = tempObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        panelArray[0].GetComponentInChildren<Text>().text = gameMaster.resMoney.ToString();
+        panelArray[1].GetComponentInChildren<Text>().text = gameMaster.resElectricity.ToString();
+        panelArray[2].GetComponentInChildren<Text>().text = gameMaster.resWorkforce.ToString();
+        panelArray[3].GetComponentInChildren<Text>().text = gameMaster.resFood.ToString();
+    }
+
+    GameMasterScript gameMaster { get { return GameObject.Find("GameMaster").GetComponent<GameMasterScript>(); } }
 }
+
