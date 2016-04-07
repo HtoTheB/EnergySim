fb3fbacf59230365fd19a04b444dee9661154f57
Niklas Reusch <nreusch@web.de>
2016-04-07 14:43:17 +0200

    Added Popup Panel

---
+++ Assets/Scripts/Windkraftwerk.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class Windkraftwerk : WorldObject {
+
+    bool panelopen = false;
+    public GameObject worldObjectPanel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected override void OpenInfoScreen()
+    {
+        if(!panelopen)
+        {
+            GameObject panel = (GameObject)Instantiate(worldObjectPanel,this.transform.position,Quaternion.identity);
+            panel.transform.SetParent(GameObject.Find("Canvas").transform);
+            panel.transform.localScale = new Vector3(1, 1, 1);
+            panel.SetActive(true);
+            panelopen = true;
+            Debug.Log("Opened WK Panel");
+        }
+    }
+}
