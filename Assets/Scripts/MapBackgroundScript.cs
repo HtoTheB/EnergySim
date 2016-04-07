3506f1ea0a7b7e264e8a76d6f374a42c6870ca6b
Maffi Craft <maffimaffi@hotmail.de>
2016-04-07 18:34:59 +0200

    Added movable Background

---
+++ Assets/Scripts/MapBackgroundScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBackgroundScript : MonoBehaviour {
+
+    public Vector3 deltaMouse, lastMouse;
+    public float stretchFactor = 15;
+    Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(1))
+        {
+            lastMouse = Input.mousePosition;
+            Debug.Log("Mouse down: "+ Input.mousePosition);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            deltaMouse = lastMouse - Input.mousePosition;
+
+            transform.position-= new Vector3 (camera.ScreenToWorldPoint(new Vector3((Screen.width/2) + deltaMouse.x, (Screen.height/2) + deltaMouse.y, 0)).x, camera.ScreenToWorldPoint(new Vector3((Screen.width/2) + deltaMouse.x, (Screen.height/2) + deltaMouse.y, 0)).y,0);
+
+            Debug.Log(new Vector3(camera.ScreenToWorldPoint(new Vector3((Screen.width/2) + deltaMouse.x, (Screen.height/2) + deltaMouse.y, 0)).x, camera.ScreenToWorldPoint(new Vector3((Screen.width/2) + deltaMouse.x, (Screen.height/2) + deltaMouse.y, 0)).y, 0));
+
+            lastMouse = Input.mousePosition;
+        }
+	}
+}
