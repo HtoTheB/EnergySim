bbcfa4e0d6dfc61bb2aa3e0da28d058357c46916
Maffi Craft <maffimaffi@hotmail.de>
2016-04-08 14:50:33 +0200

    Added Rotating Movement
    Changed from moving map to moving Camera
    Camera moves in direction of looking

---
+++ Assets/Scripts/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class MyMath
+    {
+        public static float degCos(float f) { return (Mathf.Cos(f * (Mathf.PI / 180))); }
+        public static float degSin(float f) { return (Mathf.Sin(f * (Mathf.PI / 180))); }
+    }
+}
